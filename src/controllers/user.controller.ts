a6b8b4b7c0bf39821946f284f8ab1166e951111f
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2023-10-07 09:10:52 +0700

    add

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,7 @@
 import { Request, Response } from "express";
-import prisma from "../libs/prisma";
 
 class UserController {
   public async index(req: Request, res: Response) {
-    const response = await prisma.user.findMany();
     return res.render("layouts/admin", { title: "Macan Ngamuk" });
   }
 }
